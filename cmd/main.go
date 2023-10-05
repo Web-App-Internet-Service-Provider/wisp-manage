dc972584ee509d9487a7db750e7bd914c5bc89e9
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2023-10-05 11:14:56 +0300

    Database connection err fix

--- cmd/main.go
+++ cmd/main.go
@@ -3,23 +3,31 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/AbdulrahmanDaud10/wisp-manage/internal/handlers"
 	"github.com/joho/godotenv"
 )
 
-func loadenv() {
+func LoadEnv() error {
 	if err := godotenv.Load("../build/.env"); err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	return nil
 }
 
 func main() {
-	fmt.Println("Welcome To WISP API!!!!")
+	fmt.Println("Web-App Internet Service Provider Management Service")
 
-	//Loading Environmental Variable
-	loadenv()
+	// Loading Environmental Variable file
+	if err := LoadEnv(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load .env file: %s\n", err)
+		os.Exit(1)
+	}
 
-	log.Fatal(handlers.RunAPI(":8090"))
-
+	if err := handlers.RunAPI(":8090"); err != nil {
+		fmt.Fprintf(os.Stderr, "Server start failure: %s\n", err)
+		os.Exit(1)
+	}
 }
